d00351f641367d72687aecebeea5231dee329209
EchoCordis <EchoCordis@users.noreply.github.com>
2020-12-04 22:58:08 +1100

    Changed name of CSS classes
    - Changed the name of "heading" class to "header".
    - Changed the name of "navbar" class to "navmenu".
    - Added !important to #currentpage to force it to be green.
    - Added JS to all pages for navbar transition.

--- navmenu.js
+++ navmenu.js
@@ -3,9 +3,9 @@ var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
-        document.getElementById("heading").style.top = "0";
+        document.getElementById("header").style.top = "0";
     } else {
-        document.getElementById("heading").style.top = "-10%";
+        document.getElementById("header").style.top = "-10%";
     }
     prevScrollPos = currentScrollPos;
 }
