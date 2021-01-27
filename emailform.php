253311f71012e5643857ad443c37075b4a4b530a
EchoCordis <EchoCordis@users.noreply.github.com>
2021-01-28 00:39:25 +1100

    Updated email form PHP
    - Added PHP variables for each of the fields.
    - Added subject line to email form.
    - Removed unnecessary ids from email form.

--- emailform.php
+++ emailform.php
@@ -1,5 +1,9 @@
 <?php
-if(isset($_POST['submit'])) {
-    
-}
+    if(isset($_POST['submit'])) {
+        $to = "donavan.le@gmail.com"
+        $from = $_POST['email']
+        $name = $_POST['name']
+        $subject = $_POST['subject']
+        $message = $_POST['message']
+    }
 ?>
