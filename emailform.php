bb5bd1aaaa0a48ed563206e4f59efacb1d8203d0
EchoCordis <EchoCordis@users.noreply.github.com>
2021-01-28 01:18:02 +1100

    Update emailform.php
    - Changed $message to include the name of sender.
    - Added basic header.

--- emailform.php
+++ emailform.php
@@ -1,9 +1,14 @@
 <?php
     if(isset($_POST['submit'])) {
-        $to = "donavan.le@gmail.com"
-        $from = $_POST['email']
-        $name = $_POST['name']
-        $subject = $_POST['subject']
-        $message = $_POST['message']
+        $to = "donavan.le@gmail.com";
+        $from = $_POST['email'];
+        $name = $_POST['name'];
+        $subject = $_POST['subject'];
+        $message = "A message from: " .$name. ".\n\n". $_POST['message'];
+
+        $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
+        $headers .= "From:" . $from;
+
+        mail($to,$subject,$message,$headers);
     }
 ?>
