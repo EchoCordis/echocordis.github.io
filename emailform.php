ce2a8373980f2f769ea3bc43694de7bab7d28d87
EchoCordis <EchoCordis@users.noreply.github.com>
2021-01-28 02:36:57 +1100

    Added validation of email to email form

--- emailform.php
+++ emailform.php
@@ -1,15 +1,17 @@
 <?php
     if(isset($_POST['submit'])) {
-        $to = "donavan.le@gmail.com";
-        $from = $_POST['email'];
-        $name = $_POST['name'];
-        $subject = "Portfolio Contact - " . $_POST['subject'];
-        $message = "A message from: " .$name. ".\n\n". $_POST['message'];
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $to = "donavan.le@gmail.com";
+            $from = $_POST['email'];
+            $name = $_POST['name'];
+            $subject = "Portfolio Contact - " . $_POST['subject'];
+            $message = "A message from: " .$name. ".\n\n". $_POST['message'];
 
-        $headers = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
-        $headers .= "From:" . $from;
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-Type: text/html; charset=utf-8' . "\r\n";
+            $headers .= "From:" . $from;
 
-        mail($to,$subject,$message,$headers);
+            mail($to,$subject,$message,$headers);
+        }
     }
 ?>
