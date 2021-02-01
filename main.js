e733355a4b0f3037b715d8e502bd7c535adb34e2
EchoCordis <EchoCordis@users.noreply.github.com>
2021-02-02 02:34:28 +1100

    Added initial version of slideshow gallery

--- main.js
+++ main.js
@@ -50,6 +50,35 @@ window.addEventListener('orientationchange', () => {
     }
 });
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
 /* document.addEventListener('click', (e) => {
     if(!menu.contains(e.target) && menu.querySelector(".menuitems_show")){
         menuToggle();
@@ -58,9 +87,6 @@ window.addEventListener('orientationchange', () => {
         console.log("Inside");
     }
 }); */
-
-/* Swup instance */
-/* const swup = new Swup(); */
 
 /* Changes navbar's BG opacity based on vertical scroll distance */
 /* window.onscroll = function() {
