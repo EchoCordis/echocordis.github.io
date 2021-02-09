36f938fbfc1a8be1aa7781f7e194b49cdd5d23f9
EchoCordis <EchoCordis@users.noreply.github.com>
2021-02-10 02:00:07 +1100

    Changed clock function
    - Changed night mode to toggle instead of just add.
    - Removed console log on button press.

--- scripts/switch-ui.js
+++ scripts/switch-ui.js
@@ -1,11 +1,11 @@
-/* window.onload = displayClock();
+window.onload = displayClock();
+
 function displayClock(){
   var display = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  document.body.innerHTML = display;
+  document.querySelector("#clock").textContent = display;
   setTimeout(displayClock, 1000); 
-} */
+}
 
 function setNightMode() {
-  document.body.classList.add("nightmode");
-  console.log("HI");
+  document.body.classList.toggle("nightmode");
 }
