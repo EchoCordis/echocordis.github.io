c3f63fd5bc3ac2ca61f1d951fe00f7a8dae1b3a4
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-08-04 15:51:44 +1000

    Fixed boss name generation

--- scripts/boss-name-generator.js
+++ scripts/boss-name-generator.js
@@ -2,11 +2,15 @@ var templateData;
 fetch("./data/boss-templates.json")
     .then(response => response.json())
     .then(data => {
-        templateData = data.urls;
+        templateData = data.templates;
+        console.log(templateData);
     });
 
+// Generate boss name using templates + user name input
 function generate() {
     var name = document.querySelector('#nameField').value
     var template = templateData[Math.floor(Math.random() * templateData.length)];
-    console.log(template);
+    // Used function to load string and template literal
+    var command = "console.log(" + template.replace('${name}', name) + ")";
+    new Function(command)();
 }
