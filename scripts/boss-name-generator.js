67ab921ff62a354576d933a9bd0a4e9c88597449
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-08-04 16:27:30 +1000

    Added function to capitalise all words in user's name

--- scripts/boss-name-generator.js
+++ scripts/boss-name-generator.js
@@ -8,9 +8,32 @@ fetch("./data/boss-templates.json")
 
 // Generate boss name using templates + user name input
 function generate() {
-    var name = document.querySelector('#nameField').value
-    var template = templateData[Math.floor(Math.random() * templateData.length)];
-    // Used function to load string and template literal
-    var command = "console.log(" + template.replace('${name}', name) + ")";
-    new Function(command)();
+    if (document.querySelector('#nameField').value.length > 0) {
+        var name = capitaliseName(document.querySelector('#nameField').value);
+        var template = templateData[Math.floor(Math.random() * templateData.length)];
+        // Used function to load string and template literal
+        var command = "console.log(" + template.replace('${name}', name) + ")";
+        new Function(command)();
+    }   
+}
+
+// Capitalises the words in the name
+function capitaliseName(name) {
+    if (name.split(' ').length >= 2) {
+        var nameArray = name.split(' ');
+        var capName = "";
+        // Loop through all parts of the name and capitalise the words
+        for (i = 0; i < nameArray.length; i++) {
+            capName += nameArray[i].charAt(0).toUpperCase();
+            capName += nameArray[i].slice(1);
+            // Doesn't add a space to end of final word
+            if (i < nameArray.length - 1) {
+                capName += " ";
+            }
+        }
+        return capName;
+    }
+    else {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
 }
